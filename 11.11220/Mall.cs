a0f27c30249411059a6d3386ae546d9cb74b9ca3
Asaf Harel <asafh270@gmail.com>
2020-09-12 19:50:25 +0300

    Added task 11.11220

---
+++ 11.11220/Mall.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace ADT_HW2
+{
+    public class Mall
+    {
+        Store[] stores; // Array of the stores
+
+        public Mall(Store[] stores)
+        {
+            this.stores = stores;
+        }
+
+        /**
+         * <summary>
+         * Check if a store exists in the mall
+         * </summary>
+         * <param name="name">The store's name</param>
+         * <returns>True if the store exist, else - false</returns>
+         **/
+        public bool IsExist(string name)
+        {
+            for (int i = 0; i < stores.Length; i++)
+            {
+                if (stores[i] != null)
+                {
+                    if (stores[i].getName() == name)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * <summary>
+         * Check if most of the stores already paid the rent for the last month
+         * </summary>
+         * <returns>True if most of the stores paid, else - false</returns>
+         **/
+        public bool AreMorePaid()
+        {
+            int totalStores = 0;
+            int paidCounter = 0;
+
+            for (int i = 0; i < stores.Length; i++)
+            {
+                if (stores[i] != null)
+                {
+                    totalStores++;
+
+                    if (stores[i].getIsPaid())
+                        paidCounter++;
+                }
+            }
+
+            if ((totalStores / 2) < paidCounter)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ 11.11220/Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ADT_HW2
+{
+    public class Store
+    {
+        private string name; // The name of the store
+        private int area; // The area of the store (in square meters)
+        private int rent; // The rental cost
+        private bool isPaid; // Does the rent was already paid for the last month;
+
+        public Store(string name, int area, int rent, bool isPaid)
+        {
+            this.name = name;
+            this.area = area;
+            this.rent = rent;
+            this.isPaid = isPaid;
+        }
+
+        /**
+         * <summary>
+         * Calculate the store's rent per meter
+         * </summary> 
+         * <returns>The sqare root of the rent</returns>
+         **/
+        public double CostForMeter()
+        {
+            return Math.Sqrt(rent);
+        }
+
+    }
+}
