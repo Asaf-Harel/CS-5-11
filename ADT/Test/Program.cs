5af4f3da82c3de3c339f802e500340527b548ef3
Asaf-Harel <asafh270@gmail.com>
2020-10-16 16:57:24 +0300

    Added ADT 11.12245 HW

--- ADT/Test/Program.cs
+++ ADT/Test/Program.cs
@@ -6,29 +6,57 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            double[] d = { 1.23, 12.3, 123.0, 0.123 };
+            Item item1 = new Item("abc", 'a');
+            Item item2 = new Item("abc", 'b');
+            Item item3 = new Item("bc", 'c');
+            Item item4 = new Item("bc", 'a');
+            Item item5 = new Item("c", 'd');
+            Item item6 = new Item("d", 'c');
 
-            Console.WriteLine(WhatWhat(d, 0));
+            Item[] items = { item1, item2, item3, item4, item5, item6 };
+
+            Console.WriteLine(WWW(items, 0));
         }
 
-        public static int WhatWhat(int n)
-        { 
-            if (n == 0) return 0;
-            else return 1 + WhatWhat(n / 10);
+        static bool WWW(Item item1, Item item2)
+        {
+            return WWW(item1, 0) == WWW(item2, 0);
         }
 
-        public static int WhatWhat(double n)
-        { 
-            if (n == 0.0) return 0;
-            else return 1 + WhatWhat(n* 10 - (int) n * 10);
+        static bool WWW(Item item, int i)
+        {
+            if (i == item.GetStr().Length)
+                return false;
+            else if (item.GetStr()[i] == item.GetChr())
+                return true;
+            else
+                return WWW(item, i + 1);
+
         }
 
-        public static int WhatWhat(double[] d, int k)
-        { 
-            if (k == d.Length) return 0;
-            else if (k % 2 == 0) return WhatWhat((int)d[k]) + WhatWhat(d, k + 1);
-            else return WhatWhat(d[k] - (int)d[k]) + WhatWhat(d, k + 1);
+        static int WWW(Item[] items, int i)
+        {
+            if (i == items.Length - 1 || i == items.Length)
+                return 0;
+            else if (WWW(items[i], items[i + 1]))
+                return 1 + WWW(items, i + 2);
+            else
+                return WWW(items, i + 2);
         }
 
+    }
+
+    class Item
+    {
+        private string str;
+        private char chr;
+
+        public Item(string s, char c) {
+            str = s;
+            chr = c;
+        }
+
+        public string GetStr() { return str; }
+        public char GetChr() { return chr; }
     }
 }
