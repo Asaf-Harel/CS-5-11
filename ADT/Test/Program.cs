39b818f6eaad4c38ec663ab3b0b5cb9ffad4e887
Asaf-Harel <asafh270@gmail.com>
2020-10-20 11:25:32 +0300

    Added 11.12320 recursion HW

--- ADT/Test/Program.cs
+++ ADT/Test/Program.cs
@@ -6,57 +6,36 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            Item item1 = new Item("abc", 'a');
-            Item item2 = new Item("abc", 'b');
-            Item item3 = new Item("bc", 'c');
-            Item item4 = new Item("bc", 'a');
-            Item item5 = new Item("c", 'd');
-            Item item6 = new Item("d", 'c');
-
-            Item[] items = { item1, item2, item3, item4, item5, item6 };
-
-            Console.WriteLine(WWW(items, 0));
+            int[] a = { 123, 234, 345, 456, 567, 678, 789 };
+            Console.WriteLine(WhatDoes(a, 0));
         }
 
-        static bool WWW(Item item1, Item item2)
+        public static int WhatDoes2(int n)
         {
-            return WWW(item1, 0) == WWW(item2, 0);
-        }
-
-        static bool WWW(Item item, int i)
-        {
-            if (i == item.GetStr().Length)
-                return false;
-            else if (item.GetStr()[i] == item.GetChr())
-                return true;
+            if (n < 100) return n % 10;
             else
-                return WWW(item, i + 1);
-
+                return WhatDoes2(n / 10);
         }
 
-        static int WWW(Item[] items, int i)
+        public static int WhatDoes1(int n)
         {
-            if (i == items.Length - 1 || i == items.Length)
-                return 0;
-            else if (WWW(items[i], items[i + 1]))
-                return 1 + WWW(items, i + 2);
+
+            if (n < 10) return n;
             else
-                return WWW(items, i + 2);
+                return WhatDoes1(n / 10);
+
         }
 
-    }
+        public static int WhatDoes(int[] a, int k)
+        {
+            if (k == a.Length) return 0;
+            else
+            if (k % 2 == 0)
+                return WhatDoes1(a[k]) + WhatDoes(a, k + 1);
+            else
+                return WhatDoes2(a[k]) + WhatDoes(a, k + 1);
 
-    class Item
-    {
-        private string str;
-        private char chr;
-
-        public Item(string s, char c) {
-            str = s;
-            chr = c;
         }
 
-        public string GetStr() { return str; }
-        public char GetChr() { return chr; }
     }
 }
