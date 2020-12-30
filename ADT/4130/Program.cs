b617c13fae3aaa375399cc7617bd5fa7b18c42b1
Asaf-Harel <asafh270@gmail.com>
2020-12-30 23:50:29 +0200

    Added 4130 ADT

---
+++ ADT/4130/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Application
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		static int biggest2digits(Queue<Char> q)
+		{
+			int biggest = 0;
+			int second = 0;
+			while (!q.IsEmpty())
+			{
+				int x = q.Head();
+				if (biggest == 0)
+					biggest = x;
+				else
+				{
+					if (x > biggest)
+					{
+						second = biggest;
+						biggest = x;
+					}
+					else if (x > second)
+					{
+						second = x;
+					}
+				}
+
+				q.Remove();
+			}
+
+			return (biggest * 10 + second);
+		}
+	}
+}
---
+++ ADT/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Queue<T>
+{
+    private Node<T> first;
+    private Node<T> last;
+
+    public Queue()
+    {
+        //מחזירה תור ריק
+        this.first = null;
+        this.last = null;
+    }
+
+    public void Insert(T x)
+    {
+        //מכניסה את הערך x לסוף התור
+        //הנחה - התור אותחל
+        Node<T> temp = new Node<T>(x);
+        if (first == null)
+            first = temp;
+        else
+            last.SetNext(temp);
+        last = temp;
+    }
+    public T Remove()
+    {
+        //מוציאה ומחזירה את הערך הנמצא בראש התור
+        //הנחה - התור אותחל ואינו ריק
+        T x = first.GetValue();
+        first = first.GetNext();
+        if (first == null)
+            last = null;
+        return x;
+    }
+    public T Head()
+    {
+        //מחזירה את הערך הנמצא בראש התור
+        //הנחה - התור אותחל ואינו ריק
+        return first.GetValue();
+    }
+
+    public bool IsEmpty()
+    {
+        //מחזירה האם התור ריק
+        //הנחה - התור אותחל
+        return first == null;
+    }
+
+    public override string ToString()
+    {
+        //מחזירה מחרוזת המתארת את תוכן התור
+        //הנחה - התור אותחל
+        string s = "[";
+        Node<T> p = this.first;
+        while (p != null)
+        {
+            s = s + p.GetValue().ToString();
+            if (p.GetNext() != null)
+                s = s + ",";
+            p = p.GetNext();
+        }
+        s = s + "]";
+        return s;
+    }
+}
