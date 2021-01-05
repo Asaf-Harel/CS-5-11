2c84a72648b4894f38d54fb0254b084cc1170dbb
Asaf-Harel <asafh270@gmail.com>
2021-01-05 19:47:39 +0200

    Added 11.11320

---
+++ ADT/11.11320/ClipRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Application
+{
+    public class ClipRate
+    {
+        private int id;
+        private int qualityRate, relevantRate, intrestRate;
+        private string phoneNum;
+
+        public ClipRate(int id, int qualityRate, int relevantRate, int intrestRate, string phoneNum)
+        {
+            this.id = id; this.qualityRate = qualityRate; this.relevantRate = relevantRate; this.intrestRate = intrestRate;
+            this.phoneNum = phoneNum;
+        }
+
+        public double RateAvg()
+        {
+            return (qualityRate + relevantRate + intrestRate) / 3.0;
+        }
+    }
+}
---
+++ ADT/11.11320/ClipRates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Application
+{
+    public class ClipRates
+    {
+        private ClipRate[] rates = new ClipRate[10000];
+        private int countUserRates = 0;
+        
+        public ClipRates() { }
+
+        public int CountRates(int id)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < countUserRates; i++)
+            {
+                if (rates[i].GetId() == id)
+                    counter++;
+            }
+
+            return counter;
+        }
+
+        public string UserWin()
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(countUserRates);
+
+            return rates[i].GetPhoneNum();
+        }
+
+        public void TopVideos()
+        {
+            int appear = 0;
+            bool isGood = false;
+
+            for (int i = 0; i < countUserRates; i++)
+            {
+                for (int j = 0; j < countUserRates; j++)
+                {
+                    if (rates[i] == rates[j]) {
+                        appear++;
+                        if (!isGood)
+                            isGood = rates[i].RateAvg() == 5.0;
+                    }
+                }
+
+                if (isGood && (4 <= appear))
+                    Console.WriteLine("Winner - " + rates[i].GetId());
+            }
+        }
+    }
+}
