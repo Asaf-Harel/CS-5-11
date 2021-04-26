64d43ff31d17e5e197763c91afa5d8ef908b3604
Asaf-Harel <asafh270@gmail.com>
2021-04-26 11:15:59 +0300

    added 11.02410

---
+++ ADT/11.02410/WeatherForecast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Application
+{
+    public class WeatherForecast
+    {
+        int temp;
+        int humdity;
+
+        public WeatherForecast() { }
+
+        public int TempLevel()
+        {
+            if (temp < 22)
+                return 0;
+            else if (temp > 22 && temp < 25)
+                return 1;
+            else if (temp > 25 && temp < 30)
+                return 2;
+            else
+                return 3;
+        }
+
+        public int TempLevelHu()
+        {
+            if (humdity > 75)
+            {
+                if (TempLevel() < 3)
+                    return TempLevel() + 1;
+            }
+            return TempLevel();
+        }
+    }
+}
---
+++ ADT/11.15280/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static Node<int> IndexSequence(Node<Node<int>> p)
+        {
+            Node<int> first = null, last = null;
+            int x;
+
+            while (p != null)
+            {
+                x = SequenceLength(p.GetValue());
+
+                if (first == null) { first = new Node<int>(x); last = first; }
+                else { last.SetNext(new Node<int>(x)); last = last.GetNext(); }
+
+                p = p.GetNext();
+            }
+            return first;
+        }
+
+        static int SequenceLength(Node<int> p)
+        {
+            int x, index = 1, counter = 0;
+
+            while (p != null)
+            {
+                x = p.GetValue();
+
+                if (SumDigits(x) > SumDigits(index))
+                    counter++;
+
+                p = p.GetNext();
+                index++;
+            }
+            return counter;
+        }
+
+        static int SumDigits(int n)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
